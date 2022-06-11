f101f274d9c5847221a7101c1facc71bdeac9f3d
Kalashkov Paul <kalashkovpaul@yandex.ru>
2022-06-11 11:46:33 +0300

    Changed db.sql

--- db/db.sql
+++ db/db.sql
@@ -1,49 +1,40 @@
 CREATE extension IF NOT EXISTS CITEXT;
-DROP TABLE IF EXISTS forums CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS forum_users CASCADE;
+DROP TABLE IF EXISTS forums CASCADE;
 DROP TABLE IF EXISTS threads CASCADE;
-DROP TABLE IF EXISTS votes CASCADE;
 DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS forum_users CASCADE;
+DROP TABLE IF EXISTS votes CASCADE;
+
+CREATE TABLE users (
+    id          SERIAL  UNIQUE,
+    nickname    CITEXT  NOT NULL PRIMARY KEY,
+    email       CITEXT  NOT NULL UNIQUE,
+    fullname    TEXT    NOT NULL,
+    about       TEXT    NOT NULL
+);
+
+CREATE INDEX ON users(nickname, email);
 
 CREATE TABLE forums (
     id      SERIAL,
+    slug    CITEXT          PRIMARY KEY,
     title   TEXT            NOT NULL,
     "user"  CITEXT          NOT NULL,
-    slug    CITEXT          PRIMARY KEY,
     posts   INT             NOT NULL DEFAULT 0,
     threads INT             NOT NULL DEFAULT 0 
 );
 
 CREATE INDEX ON forums USING hash (slug);
 
-CREATE TABLE users (
-    id          SERIAL  UNIQUE,
-    nickname    CITEXT  NOT NULL PRIMARY KEY,
-    fullname    TEXT    NOT NULL,
-    about       TEXT    NOT NULL,
-    email       CITEXT  NOT NULL UNIQUE
-);
-
-CREATE INDEX ON users(nickname, email);
-
-CREATE TABLE forum_users (
-  userId              INT REFERENCES users(id),
-  forumSlug CITEXT    NOT NULL,
-  username CITEXT     NOT NULL,
-  CONSTRAINT forum_users_username_forumSlug UNIQUE(forumSlug, username)
-);
-
-CREATE INDEX ON forum_users(username);
-
 CREATE TABLE threads (
     id      SERIAL          PRIMARY KEY,
+    slug    CITEXT          UNIQUE,
     title   TEXT            NOT NULL,
     author  CITEXT          NOT NULL REFERENCES users(nickname),
     forum   CITEXT          NOT NULL REFERENCES forums(slug),
     message TEXT            NOT NULL,
     votes   INT DEFAULT 0   NOT NULL,
-    slug    CITEXT          UNIQUE,
     created TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
@@ -52,23 +43,16 @@ CREATE INDEX ON threads(forum, author);
 CREATE INDEX ON threads(id, slug);
 CREATE INDEX ON threads(forum, created);
 
-CREATE TABLE IF NOT EXISTS votes (
-  user_id   CITEXT REFERENCES users(nickname)   NOT NULL,
-  thread_id INT REFERENCES threads(id)          NOT NULL,
-  voice     INT                                 NOT NULL,
-  CONSTRAINT votes_user_thread_unique UNIQUE (user_id, thread_id)
-);
-
 CREATE TABLE posts (
     id          SERIAL      PRIMARY KEY,
     parent_id   INT,
+    path        INTEGER ARRAY,
     author      CITEXT      NOT NULL REFERENCES users(nickname),
-    message     TEXT        NOT NULL,
-    edited      BOOLEAN     DEFAULT FALSE,
     forum       CITEXT      NOT NULL,
     thread_id   INT         NOT NULL,
-    created  TIMESTAMP WITH TIME ZONE DEFAULT now(),
-    path        INTEGER ARRAY
+    message     TEXT        NOT NULL,
+    edited      BOOLEAN     DEFAULT FALSE,
+    created  TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
 CREATE INDEX ON posts(thread_id, created, id);
@@ -79,7 +63,23 @@ CREATE INDEX ON posts(id);
 CREATE INDEX ON posts(thread_id);
 CREATE INDEX ON posts(id, parent_id, path, thread_id , message, edited, created, forum, author);
 
--- votes counter
+CREATE TABLE forum_users (
+  userId              INT REFERENCES users(id),
+  forumSlug CITEXT    NOT NULL,
+  username CITEXT     NOT NULL,
+  CONSTRAINT forum_users_username_forumSlug UNIQUE(forumSlug, username)
+);
+
+CREATE INDEX ON forum_users(username);
+
+CREATE TABLE IF NOT EXISTS votes (
+  user_id   CITEXT REFERENCES users(nickname)   NOT NULL,
+  thread_id INT REFERENCES threads(id)          NOT NULL,
+  voice     INT                                 NOT NULL,
+  CONSTRAINT votes_user_thread_unique UNIQUE (user_id, thread_id)
+);
+
+-- счетчик воутов
 CREATE OR REPLACE FUNCTION vote_insert()
   RETURNS TRIGGER AS $vote_insert$
     BEGIN
@@ -93,7 +93,7 @@ $vote_insert$  LANGUAGE plpgsql;
 DROP TRIGGER IF EXISTS vote_insert ON votes;
 CREATE TRIGGER vote_insert AFTER INSERT ON votes FOR EACH ROW EXECUTE PROCEDURE vote_insert();
 
--- votes editor
+-- изменение воута
 CREATE OR REPLACE FUNCTION vote_update() RETURNS TRIGGER AS $vote_update$
 BEGIN
   IF OLD.voice = NEW.voice
@@ -111,7 +111,7 @@ $vote_update$ LANGUAGE  plpgsql;
 DROP TRIGGER IF EXISTS vote_update ON votes;
 CREATE TRIGGER vote_update AFTER UPDATE ON votes FOR EACH ROW EXECUTE PROCEDURE vote_update();
 
--- vote path and parent check
+-- путь поста и проверка на родителя
 CREATE OR REPLACE FUNCTION path() RETURNS TRIGGER AS $path$
     DECLARE
         parent_path INT[];
@@ -136,7 +136,7 @@ $path$ LANGUAGE  plpgsql;
 DROP TRIGGER IF EXISTS path_trigger ON posts;
 CREATE TRIGGER path_trigger BEFORE INSERT ON posts FOR EACH ROW EXECUTE PROCEDURE path();
 
--- threads counter
+-- счетчик тредов в форуме
 CREATE OR REPLACE FUNCTION threads_forum_counter()
   RETURNS TRIGGER AS $threads_forum_counter$
     BEGIN
